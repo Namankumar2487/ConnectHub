8e21d54887e8b3e2c704fd26ecf1ede36c59a5f8
Nkay2487 <105193983+Nkay2487@users.noreply.github.com>
2022-06-17 12:40:31 +0530

    Add files via upload

---
+++ base/api/serializers.py
@@ -0,0 +1,9 @@
+# they take a data and conevrt into json format. python object into json data
+from rest_framework. serializers import ModelSerializer
+from base.models import Room
+
+class  RoomSerializer(ModelSerializer):
+    class Meta:
+        model=Room
+        fields='__all__'
+
---
+++ base/api/views.py
@@ -0,0 +1,26 @@
+from pickle import TRUE
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from base.models import Room
+from .serializers import RoomSerializer
+
+@api_view(['GET'])
+def getRoutes(request):
+    routes =[
+        'GET/ api/',
+        'GET /api/rooms',
+        'GET /api/rooms/:id'
+    ]
+    return Response( routes)
+
+@api_view(['GET'])
+def getRooms(request):
+    rooms=Room.objects.all()
+    serializer=RoomSerializer(rooms, many=True)  # many for multiple objects to serialize or one
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def getRoom(request,pk):
+    room=Room.object.get(id=pk)
+    serializer=RoomSerializer(room, many=False)  # many for multiple objects to serialize or one
+    return Response(serializer.data)
